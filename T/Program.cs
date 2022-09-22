1c86e9d9557a1f1b3f6f5772a5832888aeb7f862
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-09-22 16:16:52 +0200

    Initial commit

---
+++ T/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// string name = Console.ReadLine();
+
+// Console.WriteLine(name);
+
+// SayHello();
+
+string a = AskYesNo("Are you stupid?");
+
+if (a == "y")
+{
+  Console.WriteLine("GOOD");
+}
+
+Console.ReadLine();
+
+static void SayHello()
+{
+  Console.WriteLine("HELLO");
+}
+
+static string AskYesNo(string questionText)
+{
+  Console.WriteLine(questionText);
+  Console.WriteLine("[y/n]");
+
+  string answer = Console.ReadLine().ToLower();
+
+  return answer;
+}
